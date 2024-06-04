937f41d722e302fb37e31f9644c28b1a3a4396c2
quoccuongLE <quoccuong.le@outlook.com>
2024-06-04 07:59:53 +0200

    feat: Add lock conda

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 VENV_DIR=${1:-".venv/llama_index_app"}
-conda env create --solver=classic -f conda.yaml --prefix $VENV_DIR
+if [ -f lock.conda.yaml ]; then
+    conda env create -f lock.conda.yaml --prefix $VENV_DIR
+else
+    conda env create --solver=classic -f conda.yaml --prefix $VENV_DIR
+fi
